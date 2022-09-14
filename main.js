ba01035d244188624eb4f96c9cd8d5cba88c4c9d
Bev Feitosa <bevely1407@gmail.com>
2022-09-14 17:06:29 -0300

    versão inicial da funcionalidade dos botoes

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
-const botao = document.querySelector('#cardSona')
+const sona = document.getElementById("cardSona");
+sona.onclick = () => trocarTema("Sona");
 
-function trocaTema () {
-    const caixa = document.querySelector('#caixa');
-    console.log(caixa.classList);
-    caixa.classList.add('bg-red');
-    caixa.classList.remove('bg-aqua');
-}
+const kaisa = document.getElementById("cardKaisa");
+kaisa.onclick = () => trocarTema("Kaisa");
 
-botao.onclick = function () {
-    trocaTema();
+const taliyah = document.getElementById("cardTaliyah");
+taliyah.onclick = () => trocarTema("Taliyah");
+
+const akali = document.getElementById("cardAkali");
+akali.onclick = () => trocarTema("Akali");
+
+const corEscura = document.querySelectorAll(".caixaDeTexto");
+const fundo = document.getElementById("caixaBody");
+const corClara = document.getElementById("body");
+const cartaoAtivo = document.querySelector(".card-ativo");
+
+function trocarTema(guardia) {
+    let cartaoAtivo = document.querySelector(".card-ativo");
+    let cartaoClicado = document.getElementById(`card${guardia}`);
+    
+    for (const elementos of corEscura) {
+        elementos.classList.remove("bgSonaEscuro");
+        elementos.classList.remove("bgKaisaEscuro");
+        elementos.classList.remove("bgAkaliEscuro");
+        elementos.classList.remove("bgTaliyahEscuro");
+        elementos.classList.add(`bg${guardia}Escuro`);
+    }
+
+    fundo.classList.remove("bg-imgSona");
+    fundo.classList.remove("bg-imgKaisa");
+    fundo.classList.remove("bg-imgAkali");
+    fundo.classList.remove("bg-imgTalyah");
+    fundo.classList.add(`bg-img${guardia}`);
+
+    corClara.classList.remove("bgSonaClaro");
+    corClara.classList.remove("bgKaisaClaro");
+    corClara.classList.remove("bgAkaliClaro");
+    corClara.classList.remove("bgTaliyahClaro");
+    corClara.classList.add(`bg${guardia}Claro`);
+
+    cartaoAtivo.classList.remove("card-ativo");
+    cartaoAtivo.classList.add("card");
+
+    cartaoClicado.classList.remove("card");
+    cartaoClicado.classList.add("card-ativo");
 }
